b1e9b6de28ad5e202cf391134497d800a9abc52c
Robert Jackson <robert.w.jackson@me.com>
2014-10-16 13:55:02 -0400

    Ensure fixtures are returned asynchronously.
    Without this change fixtures are returned within the same run-loop
    (which is unlike a normal `$.ajax` request).
    
    In some cases, this leads to passing tests but broken apps (because in
    the "real world" the `$.ajax` will be delayed by a small amount).

--- dist/globals/main.js
+++ dist/globals/main.js
@@ -75,9 +75,9 @@ exports.lookupFixture = lookupFixture;function makePromise(settings) {
     var fixture = lookupFixture(settings.url);
     if (fixture) {
       if (fixture.textStatus === 'success' || fixture.textStatus == null) {
-        return Ember.run(null, resolve, fixture);
+        return Ember.run.later(null, resolve, fixture);
       } else {
-        return Ember.run(null, reject, fixture);
+        return Ember.run.later(null, reject, fixture);
       }
     }
     settings.success = makeSuccess(resolve);
